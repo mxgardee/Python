4818544cb7de6284dd055caf3054182137e3589f
Muhammed <mxgardeee@gmail.com>
2021-03-05 09:08:42 +0200

    Python_bible

---
+++ Python Bible/Project5.py
@@ -0,0 +1,24 @@
+films = {
+	"Findinf Dory" : [3:5],
+	"Bourne" : [18,5],
+	"Tarzan" : [15,5],
+	'Ghost Busters': [12,5]
+}
+
+while True:
+
+	choice = input("What film would you like to watch?").strip().tiltle()
+
+	if choice in films:
+		age = int(input('How old are you?:').strip())
+
+		if age >= films[choice][0]:
+
+			if films[choice][1]>0:
+				print('Enjoy the film!')
+				films[choice][1] = films[choice][1] - 1
+		else:
+			print('You are too young to see that film!')
+
+	else:
+		print("We don't have that film...")
